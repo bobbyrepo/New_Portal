34a268e94a3665967bb99e046de7be09a41acc38
SC00bidu <you@example.com>
2024-11-18 13:22:25 +0530

    added links to carousel card and carousel list

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom';
 
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
@@ -15,62 +16,64 @@ const CarouselCard: FC<CarouselCardProps> = ({ topHeadlines, active, toggleActiv
 
     return (
         <Box sx={{ position: "relative" }}>
+            <Link to={topHeadlines[active]?.url}>
 
-            <Card className="grid md:grid-cols-3 lg:grid-cols-2 shadow-none border-2">
-                <Box className='col-span-2 lg:col-span-1 relative md:h-[360px] h-[280px] object-cover'>
-                    <CardMedia
-                        component="img"
-                        className='h-full'
-                        image={topHeadlines[active]?.urlToImage}
-                    />
-                    <Box
-                        className="_carouselGradient"
-                        sx={{
-                            position: 'absolute',
-                            bottom: 0,
-                            height: '36px',
-                            width: '100%'
-                        }}
-                    />
-                    <Typography gutterBottom
-                        className='absolute bottom-0 px-6 font-serif
+                <Card className="grid md:grid-cols-3 lg:grid-cols-2 shadow-none border-2">
+                    <Box className='col-span-2 lg:col-span-1 relative md:h-[360px] h-[280px] object-cover'>
+                        <CardMedia
+                            component="img"
+                            className='h-full'
+                            image={topHeadlines[active]?.urlToImage}
+                        />
+                        <Box
+                            className="_carouselGradient"
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                height: '36px',
+                                width: '100%'
+                            }}
+                        />
+                        <Typography gutterBottom
+                            className='absolute bottom-0 px-6 font-serif
                         text-white md:text-[22px] sm:text-xl text-lg leading-8 line-clamp-3'
-                    >
-                        {topHeadlines[active]?.title}
-                    </Typography>
-                </Box>
-
-                <CardContent className='relative'>
-                    <Typography gutterBottom className='sm:text-xl text-lg font-serif line-clamp-4'>
-                        {topHeadlines[active]?.description}
-                    </Typography>
-                    <Typography variant="body2"
-                        className='sm:text-lg text-base font-serif line-clamp-4'
-                        sx={{ color: 'text.secondary' }}
-                    >
-                        {topHeadlines[active]?.content?.substring(0, 200)}
-                    </Typography>
-                    <Typography variant="body2" className="md:absolute bottom-1 w-full sm:text-lg text-base ">
-                        <Typography className='truncate'>Source : {topHeadlines[active]?.source.name}</Typography>
-                        <Typography>Published Date :
-                            {new Date(topHeadlines[active]?.publishedAt).toLocaleDateString()}
+                        >
+                            {topHeadlines[active]?.title}
                         </Typography>
-                    </Typography>
-                </CardContent>
+                    </Box>
 
-            </Card>
+                    <CardContent className='relative'>
+                        <Typography gutterBottom className='sm:text-xl text-lg font-serif line-clamp-4'>
+                            {topHeadlines[active]?.description}
+                        </Typography>
+                        <Typography variant="body2"
+                            className='sm:text-lg text-base font-serif line-clamp-4'
+                            sx={{ color: 'text.secondary' }}
+                        >
+                            {topHeadlines[active]?.content?.substring(0, 200)}
+                        </Typography>
+                        <Typography variant="body2" className="md:absolute bottom-1 w-full sm:text-lg text-base ">
+                            <Typography className='truncate'>Source : {topHeadlines[active]?.source.name}</Typography>
+                            <Typography>Published Date :
+                                {new Date(topHeadlines[active]?.publishedAt).toLocaleDateString()}
+                            </Typography>
+                        </Typography>
+                    </CardContent>
 
-            <KeyboardArrowLeftIcon
-                onClick={() => toggleActive('prev')}
-                className='absolute text-4xl top-1/2 left-0 -translate-y-1/2
+                </Card>
+
+                <KeyboardArrowLeftIcon
+                    onClick={() => toggleActive('prev')}
+                    className='absolute text-4xl top-1/2 left-0 -translate-y-1/2
                 text-white bg-neutral-800 rounded-full mx-2'
-            />
-            <KeyboardArrowRightIcon
-                onClick={() => toggleActive('next')}
-                className='absolute text-4xl top-1/2 right-0 -translate-y-1/2
+                />
+                <KeyboardArrowRightIcon
+                    onClick={() => toggleActive('next')}
+                    className='absolute text-4xl top-1/2 right-0 -translate-y-1/2
                 text-white bg-neutral-800 rounded-full mx-2'
-            />
+                />
 
+            </Link>
         </Box >
     )
 }
--- src/components/CarouselList.tsx
+++ src/components/CarouselList.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom';
 
 import { Grid2, Card, CardMedia, CardContent, Typography, Box } from '@mui/material'
 import { NewsCardsType } from '../utils/Types'
@@ -29,20 +30,24 @@ const CarouselList: FC<CarouselListProps> = ({ active, topHeadlines }) => {
         <Grid2 className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 mt-8">
             {nextFiveHeadlines.map((item, ind) =>
                 <Grid2 key={ind}>
-                    <Card className="relative h-fit">
-                        <CardMedia
-                            component="img"
-                            className='w-full aspect-[16/10] object-cover'
-                            image={topHeadlines[item]?.urlToImage}
-                        />
-                        <Box className="absolute -bottom-3  h-36 w-full _carouselGradient"></Box>
+                    <Link to={topHeadlines[item]?.url}>
 
-                        <CardContent className='p-2 absolute bottom-0 text-white' >
-                            <Typography className='font-serif md:text-[17px] sm:text-[15px] text-[14px] line-clamp-3'>
-                                {topHeadlines[item]?.title}
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                        <Card className="relative h-fit">
+                            <CardMedia
+                                component="img"
+                                className='w-full aspect-[16/10] object-cover'
+                                image={topHeadlines[item]?.urlToImage}
+                            />
+                            <Box className="absolute -bottom-3  h-36 w-full _carouselGradient"></Box>
+
+                            <CardContent className='p-2 absolute bottom-0 text-white' >
+                                <Typography className='font-serif md:text-[17px] sm:text-[15px] text-[14px] line-clamp-3'>
+                                    {topHeadlines[item]?.title}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+
+                    </Link>
                 </Grid2>
             )}
         </Grid2>
